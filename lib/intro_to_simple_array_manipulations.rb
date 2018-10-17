a570d1198240e51b246af49b1905e724d05768c0
Simon Lee <simonlee0306@gmail.com>
2018-10-17 20:38:43 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,16 +2,18 @@ def using_push(array,string)
   array.push(string)
 end
 
-def using_unshift(array)
-
+def using_unshift(array,string)
+  array.unshift(string)
 end
 
 def using_pop(array)
-
+  array.pop!
 end
 
 def pop_with_args(array)
-
+  new_array = []
+  new_array.push(array.pop!)
+  new_array.push(array.pop!)
 end
 
 def using_unshift(array)
