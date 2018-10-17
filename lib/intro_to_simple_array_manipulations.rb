9f515cea144dc1c27724a9b18826a06981e42169
Simon Lee <simonlee0306@gmail.com>
2018-10-17 20:36:20 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array,string)
+  array.push(string)
+end
+
+def using_unshift(array)
+
+end
+
+def using_pop(array)
+
+end
+
+def pop_with_args(array)
+
+end
+
+def using_unshift(array)
+
+end
+
+def shift_with_args(array)
+
+end
+
+def using_concat(array)
+
+end
+
+def using_insert(array)
+
+end
+
+def using_uniq(array)
+
+end
+
+def using_flatten(array)
+
+end
+
+def using_delete(array)
+
+end
+
+def using_delete_at(array)
+
+end
